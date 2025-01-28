551e92c46334e10fe0032e52e73a1da1f8659d28
Anna <anna.v98@gmail.com>
2025-01-28 14:12:50 +0900

    Adding Task_1-3.py for homework_5, flake8 corrected

--- homework/anna_vershinina/homework_5/Task_1-3.py
+++ homework/anna_vershinina/homework_5/Task_1-3.py
@@ -11,10 +11,10 @@ output_3 = 'результат работы программы: 9'
 num_1 = int(output_1[-2:]) + 10  # did not use index
 num_2 = int(
     output_2[output_2.index('514'):]
-    ) + 10  # used index to find a position of '514' using slice and save it as int
+) + 10  # used index to find a position of '514' using slice and save it as int
 num_3 = int(
     output_3[output_3.index(': ') + 2:]
-    ) + 10  # used index to find a position of ':' and start the slice from the next position
+) + 10  # used index to find a position of ':' and start the slice from the next position
 
 print(num_1, num_2, num_3)
 
@@ -25,4 +25,4 @@ subjects = ['math', 'biology', 'geography']
 print(
     f'Students {", ".join(students)} study these subjects:'
     f' {", ".join(subjects)}'
-    )
+)
