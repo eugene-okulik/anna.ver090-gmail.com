b8585689a9a8fb7888a738dc2f125d2edd338304
Anna <anna.v98@gmail.com>
2025-02-12 23:02:52 +0900

    Corrected flake8, removed unnecessary parameter, and added comments in File_1

--- homework/anna_vershinina/homework_11/File_1.py
+++ homework/anna_vershinina/homework_11/File_1.py
@@ -1,4 +1,4 @@
-# First class
+# The first class
 class Book:
     page_material = 'бумага'
     text_presence = True
@@ -11,7 +11,7 @@ class Book:
         self.ISBN = ISBN
         self.is_reserved = is_reserved
 
-
+# The second class
 class SchoolBook(Book):
 
     def __init__(self, book_name, book_author, page_number,
@@ -23,7 +23,7 @@ class SchoolBook(Book):
         self.has_tasks = has_tasks
 
 
-# Fist objects
+# The objects for the first class
 books = [
     Book('Идиот', 'Достоевский', 500, 9788420608051),
     Book('Мастер и Маргарита', 'Булгаков', 350, 9788420606840),
@@ -34,36 +34,36 @@ books = [
 
 books[-1].is_reserved = True
 
-# Second objects
+# The objects for the second class
 text_books = [
     SchoolBook('Алгебра', 'Иванов', 200,
-               9781234567890, 'Математика', 9, has_tasks=True),
+               9781234567890, 'Математика', 9),
     SchoolBook('Геометрия', 'Атанасян', 180,
-               9780987654321, 'Математика', 8, has_tasks=True),
+               9780987654321, 'Математика', 8),
     SchoolBook('Русский язык', 'Коровин', 250,
                9781112223334, 'Литература', 7, has_tasks=False),
 ]
 
 text_books[0].is_reserved = True
 
-# The First print
+# The first print
 print(*[
     f"Название: {book.book_name}, "
     f"Автор: {book.book_author}, "
     f"страниц: {book.page_number}, "
-    f"материал: {book.page_material}" +
-    (", зарезервирована" if book.is_reserved else "")
+    f"материал: {book.page_material}"
+    + (", зарезервирована" if book.is_reserved else "")
     for book in books
 ], sep='\n')
 
 print()
-# The Second print
+# The second print
 print(*[
     f"Название: {tbook.book_name}, "
     f"Автор: {tbook.book_author}, "
     f"страниц: {tbook.page_number}, "
     f"предмет: {tbook.subject}, "
-    f"класс: {tbook.class_number}" +
-    (", зарезервирована" if tbook.is_reserved else "")
+    f"класс: {tbook.class_number}"
+    + (", зарезервирована" if tbook.is_reserved else "")
     for tbook in text_books
 ], sep='\n')
